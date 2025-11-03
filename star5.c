76af43486af91fbb9a7533614e155edb403a5829
iru-han <study.iru@gmail.com>
2025-11-03 17:06:11 +0900

    Update star5.c

--- star5.c
+++ star5.c
@@ -12,18 +12,18 @@ int main() {
 	int half = (N + 1) / 2;
 	int space = half;
 	
-	for (int i = 0; i < N; i++) { // ¼¼·Î
-		if (i < half) {
+	for (int i = 0; i < N; i++) { // 세로
+		if (i < half) { // 반절 보다 작을 때에는 띄어쓰기 제거
 			space--;
 		}
-		else {
+		else { // 반절 보다 작을 때에는 띄어쓰기 추가
 			space++;
 		}
 
 		int total_star = N - (space*2);
 		int star_count = 0;
 
-		for (int j = 0; j < N; j++) { // °¡·Î
+		for (int j = 0; j < N; j++) { // 가로
 			if (j < space) {
 				printf(" ");
 			}
@@ -42,3 +42,4 @@ int main() {
 	return 0;
 
 }
+
