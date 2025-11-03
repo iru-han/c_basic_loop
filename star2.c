53531081d47e0db2d2f1d7901176630e2927fed0
iru-han <study.iru@gmail.com>
2025-11-03 17:04:05 +0900

    Restore star2.c with star pattern printing

--- star2.c
+++ star2.c
@@ -1,14 +1,21 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//int main() {
-//
-//	for (int i = 0; i < 5; i++) {
-//		for (int j = 0; j < 5 - i; j++) {
-//			printf("*");
-//		}
-//		printf("\n");
-//	}
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// *****
+// ****
+// ***
+// **
+// *
+
+int main() {
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5 - i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+
+}
