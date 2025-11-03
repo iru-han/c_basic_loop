b3bd8cf13f2460e63f410bf22bc9191ff19d7326
iru-han <study.iru@gmail.com>
2025-11-03 17:03:42 +0900

    Refactor star1.c to include main function and print stars

--- star1.c
+++ star1.c
@@ -1,18 +1,24 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//int main() {
-//
-// // 입력받기
-// int N;
-// scanf("%d", &N);
-// 
-//	for (int i = 0; i < 5; i++) {
-//		for (int j = 0; j < i+1; j++) {
-//			printf("*");
-//		}
-//		printf("\n");
-//	}
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// *
+// **
+// ***
+// ****
+// *****
+
+int main() {
+
+int N;
+scanf("%d", &N);
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < i+1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+
+}
