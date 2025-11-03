1095122994219418b47ab33d8c8cf9c03aea6f8c
iru-han <study.iru@gmail.com>
2025-11-03 15:14:29 +0900

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	
+	//for (int i = 65; i <= 90; i++) {
+	//	printf("%c ", i);
+	//}
+
+	for (int i = 'A'; i <= 'Z'; i++) {
+		printf("%c ", i);
+	}
+
+	printf("\n");
+
+	//for (int i = 97; i <= 122; i++) {
+	//	printf("%c ", i);
+	//}
+
+	for (int i = 'a'; i <= 'z'; i++) {
+		printf("%c ", i);
+	}
+
+}
